62ea0836360d3b2113a37182c62439fe94a2a1af
Janan Gandhi <janangandhi94@gmail.com>
2017-02-23 03:43:49 +0530

    Work in progress for delete dishes. work remaining for the listner. Delete the fragment_viewdishes later

--- app/src/main/java/com/gandhis/foodchain/ViewDishes.java
+++ app/src/main/java/com/gandhis/foodchain/ViewDishes.java
@@ -6,15 +6,17 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.AbsListView;
 import android.widget.CursorAdapter;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
-
-import java.sql.SQLException;
 
 public class ViewDishes extends ListFragment {
 
@@ -22,32 +24,29 @@ public class ViewDishes extends ListFragment {
     private DishesSource dishSource;
     private SQLiteDatabase db;
     private Cursor cursor;
+    private CursorAdapter list;
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         FoodChainDatabaseHelper dbhelper = new FoodChainDatabaseHelper(getActivity());
         db=dbhelper.getWritableDatabase();
-        if(db.isOpen())
-            Log.w("Status of db","Open" );
-        try
-        {
-            cursor= db.query(dbhelper.TABLE_NAME,
+        try {
+            cursor = db.query(dbhelper.TABLE_NAME,
                     new String[]{dbhelper.COLUMN_ID, dbhelper.COULUMN_NAME},
                     null,
                     null,
                     null,
                     null,
                     null);
-            Log.w("String","Reached here");
-            CursorAdapter list = new SimpleCursorAdapter(getActivity(),
-                    android.R.layout.simple_list_item_1,
+            list = new SimpleCursorAdapter(getActivity(),
+                    android.R.layout.simple_list_item_activated_1,
                     cursor,
                     new String[]{dbhelper.COULUMN_NAME },
                     new int[]{android.R.id.text1},
                     0);
-            setListAdapter(list);
+
 
         }
         catch(Exception e) {
@@ -55,16 +54,81 @@ public class ViewDishes extends ListFragment {
             Toast toast = Toast.makeText(getActivity(), "Database unavailable", Toast.LENGTH_SHORT);
             toast.show();
         }
+    }
+
+    @Override
+    public void onActivityCreated (Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setAdapter();
+
+    }
+
+    public void setAdapter()
+    {
+        setListAdapter(list);
+        Log.w("adapter", "set");
+        ListView lv = getListView();
+        if(lv != null)
+            Log.w("ListView", "loaded");
+        lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        Log.w("ListView", String.valueOf(lv.getChoiceMode()));
+
+        lv.setMultiChoiceModeListener(new ModeCallback());
+
+    }
 
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
 
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
+
+
     @Override
     public void onStop() {
         super.onStop();
         cursor.close();
+    }
+
+
+    private class ModeCallback implements ListView.MultiChoiceModeListener
+    {
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        mode.getMenuInflater().inflate(R.menu.home_page, menu);
+        mode.setTitle("Select Items");
+        return true;
+    }
+
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return true;
+    }
+
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+        return true;
+    }
+
+    public void onDestroyActionMode(ActionMode mode) {
+    }
+
+    public void onItemCheckedStateChanged(ActionMode mode,
+                                          int position, long id, boolean checked) {
+        final int checkedCount = getListView().getCheckedItemCount();
+        Log.w("checked", "in");
+        switch (checkedCount) {
+            case 0:
+                mode.setSubtitle(null);
+                break;
+            case 1:
+                mode.setSubtitle("One item selected");
+                break;
+            default:
+                mode.setSubtitle("" + checkedCount + " items selected");
+                break;
+        }
+    }
     }
 }
--- app/src/main/java/com/gandhis/foodchain/dishDisplay.java
+++ app/src/main/java/com/gandhis/foodchain/dishDisplay.java
@@ -1,6 +1,7 @@
 package com.gandhis.foodchain;
 
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.util.Log;
@@ -9,6 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Random;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +21,8 @@ import android.widget.TextView;
 public class dishDisplay extends Fragment {
 
     private String dishName;
+    private DishesSource dishSource;
+    private List<Dish> DishesArray;
     public dishDisplay() {
         // Required empty public constructor
     }
@@ -32,12 +39,15 @@ public class dishDisplay extends Fragment {
         Log.w("Value of text before",String.valueOf(dishNameText.getText()));
         dishNameText.setText(dishName);
         Log.w("Value of text after", String.valueOf(dishNameText.getText()));
-        return inflater.inflate(R.layout.fragment_dish_display, container, false);
+        return rootView;
     }
 
-    public void findDish(View view)
+    public void findDish(View view)throws SQLException
     {
-
+        dishSource.open();
+        DishesArray=dishSource.getAllDishes();
+        TextView tv= (TextView)getView().findViewById(R.id.dishSelected);
+        tv.setText(DishesArray.get(new Random().nextInt(DishesArray.size())).getName());
     }
 
 }
